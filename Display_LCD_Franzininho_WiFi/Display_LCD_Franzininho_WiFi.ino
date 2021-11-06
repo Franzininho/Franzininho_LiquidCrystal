730e327b82603f714643f4b932c21c9804af287d
halyssonJr <halysson1007@gmail.com>
2021-11-06 11:05:39 -0300

    Atualização do software

--- Display_LCD_Franzininho_WiFi/Display_LCD_Franzininho_WiFi.ino
+++ Display_LCD_Franzininho_WiFi/Display_LCD_Franzininho_WiFi.ino
@@ -10,10 +10,10 @@
 */
 #include "Franzininho_LiquidCrystal.h" /* Arquivo de cabeçalho para a inclusão do*/
 
-Franzininho_LiquidCrystal FranzininhoWifi ( 0,1,2,3,4,5); /* Construtor */
+Franzininho_LiquidCrystal FranzininhoWifi (3,2,1,0,4,5); /* Construtor */
 
 void setup() {
-
+  FranzininhoWifi.LCD_Iniciar();
   FranzininhoWifi.LCD_Comando(LCD_Clear);
   FranzininhoWifi.LCD_Saida_String(1,2, "FRANZININHO");
   FranzininhoWifi.LCD_Saida_String(2,5, "WIFI");
--- Display_LCD_Franzininho_WiFi/Franzininho_LiquidCrystal.h
+++ Display_LCD_Franzininho_WiFi/Franzininho_LiquidCrystal.h
@@ -1,7 +1,10 @@
 #ifndef FRANZININHO_LIQUIDCRYSTAL_H
 #define FRANZININHO_LIQUIDCRYSTAL_H
 
-#include <Arduino.h>
+#include "Arduino.h"
+#include <inttypes.h>
+#include "Print.h"
+
 /* Lista de Comandos*/
 #define LCD_Clear             (0x01)
 #define LCD_Shift_LEFT        (0x18)
@@ -13,29 +16,28 @@
 #define LCD_Inicializar       (0x03)
 
 
-class Franzininho_LiquidCrystal { 
+class Franzininho_LiquidCrystal  { 
 public:
-Franzininho_LiquidCrystal(uint8_t bit_4, uint8_t bit_5, uint8_t bit_6,
-                          uint8_t bit_7,uint8_t rs_ctrl_pino, uint8_t en_ctrl_pino);
+Franzininho_LiquidCrystal(unsigned char bit_4, unsigned char bit_5, unsigned char bit_6,
+                          unsigned char bit_7,unsigned char rs_ctrl_pino, unsigned char en_ctrl_pino);
 
-void LCD_Iniciar (uint8_t lcd_bit4,uint8_t lcd_bit5,uint8_t lcd_bit6,
-                  uint8_t lcd_bit7,uint8_t lcd_pino_rs,uint8_t lcd_pino_en);
+void LCD_Iniciar ();
                   
-void LCD_Saida_String(uint8_t linha, uint8_t coluna,  char *texto);
-void LCD_Saida_Char(uint8_t linha, uint8_t coluna,  char caracter);
+void LCD_Saida_String(unsigned char linha, unsigned char coluna,  char *texto);
+void LCD_Saida_Char(unsigned char linha, unsigned char coluna, unsigned char caracter);
 void LCD_Comando(unsigned char comandoLCD);
+void LCD_Escreve_Dado( unsigned char dado);
+void LCD_Saida_Cp( char *ponteiro);
+void LCD_Saida_Cp_Char ( unsigned char caracter);
 
 private:
 
 void LCD_Escreve_Nibble( unsigned char dado);
-void LCD_Escreve_Dado( unsigned char dado);
-void LCD_Saida_Cp( char *ponteiro);
 void LCD_Habilita(void);
-void LCD_Saida_Cp_Char ( char caracter);
-void LCD_Envia_Dado(unsigned char dado, uint8_t modo);
+void LCD_Envia_Dado(unsigned char dado,unsigned char modo);
 
-uint8_t bit_pino[4];                                                 /*Vetor contendo os pinos de dados */
-uint8_t LCD_PINO_RS, LCD_PINO_EN; 
+unsigned char bit_pino[4];                                                 /*Vetor contendo os pinos de dados */
+unsigned char LCD_PINO_RS, LCD_PINO_EN; 
 };
 
 
